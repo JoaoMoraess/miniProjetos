abf36ef3ac79101c21a22c21c1e0fb2c836fd590
JoaoMoraess <69697696+JoaoMoraess@users.noreply.github.com>
2020-12-17 19:07:50 -0300

    Urna Eletronica com js
    creditos ao bonieky

---
+++ JavaScript09/etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: "38111",
+        nome: "Fulano de Tal",
+        partido: "ABC",
+        fotos: [{ url: "v1.jpg", legenda: "Vereador", small: false }],
+      },
+      {
+        numero: "77222",
+        nome: "Beltrano de Tal",
+        partido: "DEFG",
+        fotos: [{ url: "v2.jpg", legenda: "Vereador", small: false }],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "99",
+        nome: "Ciclano",
+        partido: "ABC",
+        vice: "Cic",
+        fotos: [
+          { url: "cp1.jpg", legenda: "Prefeito", small: false },
+          { url: "cv1.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+      {
+        numero: "84",
+        nome: "Zulano",
+        partido: "QWERTY",
+        vice: "Zul",
+        fotos: [
+          { url: "cp2.jpg", legenda: "Prefeito", small: false },
+          { url: "cv2.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+    ],
+  },
+];
